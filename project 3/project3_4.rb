6ae4c005620f845051782d127a1045e2ef087969
Pakpoom-Phoonpunt <s6201012620210@email.kmutnb.ac.th>
2021-08-20 16:19:05 +0700

    add project 3.4

---
+++ project 3/project3_4.rb
@@ -0,0 +1,10 @@
+i = 1
+f = 1.1
+i+=f
+puts "answer of i+=f is "+ String(i)    #output: answer of i+=f is 2.1
+puts i.class    #output: Float
+i = 1
+f = 1.1
+i = i+f
+puts "answer of i=i+f is "+ String(i)   #output: answer of i+=f is 2.1
+puts i.class    #output: Float
