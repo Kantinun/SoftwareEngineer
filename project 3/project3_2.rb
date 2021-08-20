49f68b3b8a9c21db2e70628874eda202a562da36
Pakpoom-Phoonpunt <s6201012620210@email.kmutnb.ac.th>
2021-08-20 15:55:27 +0700

    add project3_2

---
+++ project 3/project3_2.rb
@@ -0,0 +1,18 @@
+def ancestor(args)
+    # if args == BasicObject
+    #     return "nil"
+    # elsif [Integer , String , Float , Array , TrueClass ,FalseClass ].include? args.class
+    #     return args.class.name + ">" +ancestor(args.class)
+    # else
+    #     return args.superclass.name + ">" +ancestor(args.superclass)
+    # end
+    
+    if args == BasicObject
+        return nil
+    else 
+        return args.class.ancestors
+    end
+end
+
+
+puts ancestor(1)
