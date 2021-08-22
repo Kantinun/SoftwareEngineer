c5c0933d17bfe0a0474d99d1c6a60a9b37f0c54b
Kantinun <s6201012630011@email.kmutnb.ac.th>
2021-08-22 19:59:19 +0700

    Add project3_6

--- project 3/project3_6.rb
+++ project 3/project3_6.rb
@@ -1,41 +1,48 @@
 class Class
-  def attr_accessor_with_history(name)
-    attri_name = name
-    attri_his = "history(#{attri_name})"
-    his = []
+ 
+  def attr_accessor_with_history(*args)
+      args.each do |arg|
+          attr_name = arg.to_s
+          attr_hist_name = attr_name +'_history'
+      
     #getter
-    self.class_eval("def #{attri_name} 
-                      @#{attri_name} 
-                    end")
-    # #setter
-    # self.class_eval %Q{
-    #                 def #{attri_name}=(val)
-    #                   @#{attri_name} = val
-    #                   #{his} << val
-    #                 end
-    #                 def #{attri_his}; return #{his}; end
-    #                 }
-    
-    self.class_eval %Q{
-      def #{attri_name}=(val)
-        # add to history
-        @#{attri_his} = [nil] if @#{attri_his}.nil?
-        @#{attri_his} << val
+          self.class_eval("def #{attr_name};@#{attr_name};end")
+ 
+    #setter
+          self.class_eval %Q{
+          def #{attr_name}=(val)
+          # add to history
+              @#{attr_hist_name} = [] if @#{attr_hist_name}.nil?
+              @#{attr_hist_name} << val
  
         # set the value itself
-        @#{attri_name}=val
-      end
+              @#{attr_name}=val
+          end
  
-      def #{attri_his};@#{attri_his};end
+          def #{attr_hist_name};@#{attr_hist_name};end
  
                     }
+          class_eval do
+              def history(name)
+                  instance_variable_get("@#{name}_history")
+              end
+          end
+      end
   end
+ 
 end
 class Foo
-    attr_accessor_with_history :bar
-  end
-f = Foo.new     # => #<Foo:0x127e678>
+  attr_accessor_with_history :bar, :el
+end
+   
+f = Foo.new
 f.bar = 3
-puts f.bar
-puts f.history(:bar)
+f.bar = :wowzo
+f.bar = 'boo!'
 
+f.el = 2
+f.el = :ezy
+f.el = "Woww!"
+
+print f.history(:bar)
+print f.history(:el)
